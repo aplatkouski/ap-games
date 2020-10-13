fae387cea9f4c534a07a378e0648765b85d74fa4
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-10-13 22:16:13 +0300

    test: add tests for Reversi class
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- tests/game/conftest.py
+++ tests/game/conftest.py
@@ -1,12 +1,96 @@
-from typing import Literal
+from __future__ import annotations
+
+from typing import cast
+from typing import TYPE_CHECKING
 
 import pytest  # type: ignore
 
+from ap_games.ap_constants import EMPTY
 from ap_games.game.reversi import Reversi
+from ap_games.gameboard.gameboard import SquareGameboard
+
+if TYPE_CHECKING:
+    from typing import Any
+    from typing import Literal
+
+
+@pytest.fixture(
+    scope='session',
+    params=[
+        'get_status',
+        'place_mark',
+        'get_available_moves',
+        'get_score',
+        'default_grid',
+        'grid_axis',
+        'grid_gap',
+        'supported_players',
+        'rules',
+        'priority_coordinates',
+        'players',
+    ],
+    ids=lambda interface: f'{interface=}',
+)
+def reversi_interface(request: Any) -> str:
+    return cast(str, request.param)
+
+
+@pytest.fixture(scope='session')
+def reversi_gameboard_as_string() -> str:
+    return (
+        '        '
+        '        '
+        '   X X  '
+        '  OOOO  '
+        '  XOO   '
+        '    OX  '
+        '        '
+        '        '
+    )
+
+
+@pytest.fixture(scope='session')
+def default_gameboard_as_string() -> str:
+    return f'{EMPTY * 27}XO{EMPTY * 6}OX{EMPTY * 27}'
+
+
+@pytest.fixture(
+    scope='session',
+    params=[0, 1, 3],
+    ids=lambda players_number: f'{players_number=}',
+)
+def wrong_players_number(request: Any) -> int:
+    return cast(int, request.param)
+
+
+@pytest.fixture(
+    scope='session', params=['0', 'A', '/'], ids=lambda symbol: f'{symbol=}',
+)
+def wrong_grid_symbol(request: Any) -> str:
+    return cast(str, request.param)
+
+
+@pytest.fixture()
+def reversi_user_user(default_gameboard_as_string: str) -> Reversi:
+    """Return Reversi game with the default grid and two human players.
+
+    :param default_gameboard_as_string:  The start Reversi board as
+        string.
+
+    """
+    user: Literal['user'] = 'user'
+    return Reversi(grid=default_gameboard_as_string, player_types=(user, user))
+
+
+@pytest.fixture()
+def reversi_gameboard(reversi_gameboard_as_string: str) -> SquareGameboard:
+    return SquareGameboard(grid=reversi_gameboard_as_string)
 
 
 @pytest.fixture(scope='package')
-def reversi_user_user() -> Reversi:
-    """Return Reversi game with the default grid and two human players."""
-    user: Literal['user'] = 'user'
-    return Reversi(player_types=(user, user))
+def reversi_medium_x_easy_o(reversi_gameboard_as_string: str) -> Reversi:
+    medium: Literal['medium'] = 'medium'
+    easy: Literal['easy'] = 'easy'
+    return Reversi(
+        grid=reversi_gameboard_as_string, player_types=(medium, easy)
+    )
--- tests/game/test_reversi.py
+++ tests/game/test_reversi.py
@@ -1,44 +1,206 @@
 from __future__ import annotations
 
-from typing import cast
 from typing import TYPE_CHECKING
+
+import pytest  # type: ignore
 
 from ap_games.ap_collections import Coordinate
 from ap_games.ap_collections import GameStatus
-from ap_games.ap_typing import PlayerType
 from ap_games.game.reversi import Reversi
+from ap_games.gameboard.gameboard import SquareGameboard
 
 if TYPE_CHECKING:
+    from typing import Literal
+    from typing import Tuple
+
     from ap_games.ap_typing import Coordinates
+    from ap_games.ap_typing import PlayerType
 
 
-def test_construction() -> None:
-    user: PlayerType = cast(PlayerType, 'user')
-    reversi: Reversi = Reversi(grid=' ' * 64, player_types=(user, user))
-    assert ' ' * 64 == reversi.gameboard.grid_as_string
-    assert 'user' == reversi.players[0].type_
-    assert 'user' == reversi.players[1].type_
+class TestReversi:
+    def test_reversi_interface(
+        self, reversi_user_user: Reversi, reversi_interface: str
+    ) -> None:
+        assert hasattr(reversi_user_user, reversi_interface)
 
+    def test_gameboard(self, reversi_gameboard_as_string: str) -> None:
+        assert (
+            reversi_gameboard_as_string
+            == Reversi(
+                grid=reversi_gameboard_as_string
+            ).gameboard.grid_as_string
+        )
 
-def test_get_available_moves(reversi_user_user: Reversi) -> None:
-    available_moves: Coordinates = (
-        Coordinate(6, 5),
-        Coordinate(3, 4),
-        Coordinate(5, 6),
-        Coordinate(4, 3),
+    def test_default_gameboard(self, default_gameboard_as_string: str) -> None:
+        user: Literal['user'] = 'user'
+        assert (
+            default_gameboard_as_string
+            == Reversi(player_types=(user, user)).gameboard.grid_as_string
+        )
+
+    def test_wrong_players_number(self, wrong_players_number: int) -> None:
+        user: PlayerType = 'user'
+        player_types: Tuple[PlayerType, ...] = (user,) * wrong_players_number
+        with pytest.raises(ValueError, match='number of players') as e:
+            Reversi(player_types=player_types)  # type: ignore
+        assert 'The number of players should be 2!' == str(e.value)
+
+    def test_grid_with_underscore(self) -> None:
+        user: PlayerType = 'user'
+        assert (
+            ' ' * 64
+            == Reversi(
+                grid='_' * 64, player_types=(user, user)
+            ).gameboard.grid_as_string
+        )
+
+    def test_inappropriate_grid_character(
+        self, wrong_grid_symbol: str
+    ) -> None:
+        user: PlayerType = 'user'
+        with pytest.raises(ValueError, match='must contain') as e:
+            Reversi(grid=wrong_grid_symbol * 64, player_types=(user, user))
+        assert (
+            'Gameboard must contain only " ", "_" and symbols '
+            "from ('X', 'O')."
+        ) == str(e.value)
+
+    @pytest.mark.parametrize(
+        ('gameboard_grid', 'player_mark', 'score'),
+        [
+            ('   OOOXXX', 'X', 0),
+            ('   OOXXXX', 'X', 2),
+            ('   OOOOXX', 'X', -2),
+        ],
+        ids=lambda arg: f'{arg}',
     )
-    assert available_moves == reversi_user_user.get_available_moves()
+    def test_get_score(
+        self,
+        gameboard_grid: str,
+        player_mark: Literal['X'],
+        score: int,
+        reversi_user_user: Reversi,
+    ) -> None:
+        gameboard: SquareGameboard = SquareGameboard(grid=gameboard_grid)
+        assert score == reversi_user_user.get_score(gameboard, player_mark)
 
-
-def test_get_score(reversi_user_user: Reversi) -> None:
-    assert 0 == reversi_user_user.get_score(
-        gameboard=reversi_user_user.gameboard,
-        player_mark=reversi_user_user.players[0].mark,
+    @pytest.mark.parametrize(
+        ('gameboard_grid', 'player_mark', 'available_moves'),
+        [
+            (
+                '   OOOXXX',
+                'X',
+                (Coordinate(1, 3), Coordinate(3, 3), Coordinate(2, 3)),
+            ),
+            ('   OXOXXX', 'X', (Coordinate(1, 3), Coordinate(3, 3))),
+            ('   OXXXXX', 'X', (Coordinate(1, 3),)),
+            ('   XXXOOO', 'X', (),),
+        ],
+        ids=lambda arg: f'{arg}',
     )
+    def test_get_available_moves(
+        self,
+        gameboard_grid: str,
+        player_mark: Literal['X'],
+        available_moves: Coordinates,
+        reversi_user_user: Reversi,
+    ) -> None:
+        gameboard: SquareGameboard = SquareGameboard(grid=gameboard_grid)
+        assert available_moves == reversi_user_user.get_available_moves(
+            gameboard, player_mark
+        )
 
-
-def test_get_status(reversi_user_user: Reversi) -> None:
-    assert (
-        GameStatus(active=True, message='', must_skip=False)
-        == reversi_user_user.get_status()
+    @pytest.mark.parametrize(
+        ('gameboard_grid', 'player_mark', 'game_status'),
+        [
+            (
+                '   OOOXXX',
+                'X',
+                GameStatus(active=True, message='', must_skip=False),
+            ),
+            (
+                '   XXXOOO',
+                'X',
+                GameStatus(
+                    active=False,
+                    message='\nThe player [X] has no moves available!\n',
+                    must_skip=True,
+                ),
+            ),
+            (
+                'XXXXXXOOO',
+                'X',
+                GameStatus(active=False, message='X wins\n', must_skip=False,),
+            ),
+            (
+                'XXXOOOOOO',
+                'X',
+                GameStatus(active=False, message='O wins\n', must_skip=False,),
+            ),
+            (
+                ' OOOXXOXX',
+                'X',
+                GameStatus(active=False, message='Draw\n', must_skip=False,),
+            ),
+        ],
     )
+    def test_get_status(
+        self,
+        gameboard_grid: str,
+        player_mark: Literal['X'],
+        game_status: GameStatus,
+        reversi_user_user: Reversi,
+    ) -> None:
+        gameboard: SquareGameboard = SquareGameboard(grid=gameboard_grid)
+        assert game_status == reversi_user_user.get_status(
+            gameboard, player_mark
+        )
+
+    @pytest.mark.parametrize(
+        ('coordinate', 'player_mark', 'gameboard_grid', 'score'),
+        [
+            (Coordinate(1, 3), 'X', '   OOOXXX', 3,),
+            (Coordinate(2, 3), 'X', '   OOOXXX', 2,),
+            (Coordinate(1, 1), 'X', '   OOOXXX', 0,),
+            (Coordinate(1, 3), 'X', '   XXXOOO', 0,),
+        ],
+        ids=lambda arg: f'{arg}',
+    )
+    def test_place_mark(
+        self,
+        coordinate: Coordinate,
+        player_mark: Literal['X'],
+        gameboard_grid: str,
+        score: int,
+        reversi_user_user: Reversi,
+    ) -> None:
+        gameboard: SquareGameboard = SquareGameboard(grid=gameboard_grid)
+        assert score == reversi_user_user.place_mark(
+            coordinate, player_mark, gameboard
+        )
+
+    def test_fill_available_moves_cache(
+        self, reversi_user_user: Reversi
+    ) -> None:
+        coordinate: Coordinate = Coordinate(6, 5)
+        player_mark: Literal['X'] = 'X'
+
+        grid: str = reversi_user_user.gameboard.grid_as_string
+        reversi_user_user.place_mark(
+            coordinate=coordinate,
+            player_mark=player_mark,
+            gameboard=SquareGameboard(grid=grid),
+        )
+        assert (
+            [Coordinate(x=5, y=5)]
+            == reversi_user_user._available_moves_cache[grid, player_mark][
+                coordinate
+            ]
+        )
+        reversi_user_user.place_mark(
+            coordinate=coordinate, player_mark=player_mark,
+        )
+        assert (
+            grid,
+            player_mark,
+        ) not in reversi_user_user._available_moves_cache
